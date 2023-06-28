8d05d45042df0f015a0dcc86ae46b184479abe3e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-28 18:18:18 +0300

    add router post for create product

--- routes/api/products-router.js
+++ routes/api/products-router.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const ctrl = require('../../controllers/products-controllers');
+const validateBody = require('../../decorators/validateBody');
+const { productAddSchema } = require('../../models/product');
+// console.log(productAddSchema);
 
 const router = express.Router();
 
 router.get('/');
-router.post('/');
+router.post('/', validateBody(productAddSchema), ctrl.createProduct);
 
 module.exports = router;
