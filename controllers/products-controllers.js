293f20af37b668faa84a109feee07ba72927ddba
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-28 17:51:34 +0300

    add controller to create product

--- controllers/products-controllers.js
+++ controllers/products-controllers.js
@@ -1,8 +1,16 @@
+const Product = require('../models/product');
+const HttpError = require('../helpers/HttpError');
+const ctrl = require('../decorators/ctrlWrapper');
+
 const getAllProducts = async (req, res) => {};
 
-const createProduct = async (req, res) => {};
+const createProduct = async (req, res) => {
+  const { body } = req;
+  const product = await Product.create(body);
+  res.status(201).json(product);
+};
 
 module.exports = {
-  getAllProducts,
-  createProduct,
+  getAllProducts: ctrl(getAllProducts),
+  createProduct: ctrl(createProduct),
 };
