927b25467c9aa4471ba6a61bd3c6e9b42ef0f25f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-28 22:23:43 +0300

    add check auth to routers

--- routes/api/products-router.js
+++ routes/api/products-router.js
@@ -3,10 +3,11 @@ const ctrl = require('../../controllers/products-controllers');
 const validateBody = require('../../decorators/validateBody');
 const { productAddSchema } = require('../../models/product');
 // console.log(productAddSchema);
+const checkAuth = require('../../middlewares/check-auth');
 
 const router = express.Router();
 
-router.get('/', ctrl.getAllProducts);
-router.post('/', validateBody(productAddSchema), ctrl.createProduct);
+router.get('/', checkAuth, ctrl.getAllProducts);
+router.post('/', checkAuth, validateBody(productAddSchema), ctrl.createProduct);
 
 module.exports = router;
