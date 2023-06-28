b7f32b58e8ad98dd502aa4f0d59f8dbc747fb082
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-28 17:49:50 +0300

    complete models

--- models/product.js
+++ models/product.js
@@ -1,11 +1,26 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
 
-const productSchema = new Schema({});
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 productSchema.post('save');
 
-const productAddSchema = Joi.object({});
+const productAddSchema = Joi.object({
+  name: Joi.string().required(),
+  price: Joi.number().required(),
+});
 
 const Product = model('product', productSchema);
 
