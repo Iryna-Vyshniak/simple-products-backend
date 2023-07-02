42c9e316ef432d03079bd1bdd7a5b4503ab117e6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-02 19:57:28 +0300

    add router avatars to update user avatar

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -3,11 +3,13 @@ const ctrl = require('../../controllers/auth-controllers');
 const validateBody = require('../../decorators/validateBody');
 const { schemas } = require('../../models/user');
 const checkAuth = require('../../middlewares/check-auth');
+const upload = require('../../middlewares/upload');
 
 const router = Router();
 
 router.post('/signup', validateBody(schemas.registerSchema), ctrl.signUp);
 router.post('/signin', validateBody(schemas.loginSchema), ctrl.signIn);
 router.post('/logout', checkAuth, ctrl.logout);
+router.patch('/avatars', checkAuth, upload.single('avatar'), ctrl.updateAvatar);
 
 module.exports = router;
