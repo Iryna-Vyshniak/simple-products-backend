4d5416a6357738389874a02d2edd94517726ad4c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-12 13:40:36 +0300

    add router for get product by id

--- routes/api/products-router.js
+++ routes/api/products-router.js
@@ -3,11 +3,12 @@ const ctrl = require('../../controllers/products-controllers');
 const validateBody = require('../../decorators/validateBody');
 const { productAddSchema } = require('../../models/product');
 // console.log(productAddSchema);
-const checkAuth = require('../../middlewares/check-auth');
+const { checkAuth, isValidId } = require('../../middlewares');
 
 const router = express.Router();
 
 router.get('/', checkAuth, ctrl.getAllProducts);
+router.get('/:id', isValidId, checkAuth, ctrl.createProduct);
 router.post('/', checkAuth, validateBody(productAddSchema), ctrl.createProduct);
 
 module.exports = router;
