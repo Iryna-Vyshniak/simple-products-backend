6f3d13d4f2501adcd185230a48f0b25e28aec2e6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-12 13:37:30 +0300

    add route for current user

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,13 +2,13 @@ const { Router } = require('express');
 const ctrl = require('../../controllers/auth-controllers');
 const validateBody = require('../../decorators/validateBody');
 const { schemas } = require('../../models/user');
-const checkAuth = require('../../middlewares/check-auth');
-const upload = require('../../middlewares/upload');
+const { checkAuth, upload } = require('../../middlewares');
 
 const router = Router();
 
 router.post('/signup', validateBody(schemas.registerSchema), ctrl.signUp);
 router.post('/signin', validateBody(schemas.loginSchema), ctrl.signIn);
+router.get('/current', checkAuth, ctrl.getCurrent);
 router.post('/logout', checkAuth, ctrl.logout);
 router.patch('/avatars', checkAuth, upload.single('avatar'), ctrl.updateAvatar);
 
