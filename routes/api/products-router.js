ff1508d69850f0176e27fa9ffc501e02f35429fe
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-12 14:31:15 +0300

    add router for update field favorite

--- routes/api/products-router.js
+++ routes/api/products-router.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const ctrl = require('../../controllers/products-controllers');
 const validateBody = require('../../decorators/validateBody');
-const { productAddSchema } = require('../../models/product');
+const { schemas } = require('../../models/product');
 const { checkAuth, isValidId } = require('../../middlewares');
 
 const router = express.Router();
 
 router.get('/', checkAuth, ctrl.getAllProducts);
 router.get('/:id', isValidId, checkAuth, ctrl.createProduct);
-router.post('/', checkAuth, validateBody(productAddSchema), ctrl.createProduct);
+router.post('/', checkAuth, validateBody(schemas.productAddSchema), ctrl.createProduct);
+router.patch(
+  '/:id/favorite',
+  checkAuth,
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusProduct
+);
 
 module.exports = router;
