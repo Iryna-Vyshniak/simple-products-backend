ccdfaeae63d2c24290640bc3e181338f14e3f589
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-30 22:53:57 +0300

    add re-export upload cloud

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,6 @@
 const isValidId = require('./isValidId');
 const checkAuth = require('./check-auth');
 const upload = require('./upload');
+const uploadCloud = require('./uploadCloud');
 
-module.exports = { isValidId, checkAuth, upload };
+module.exports = { isValidId, checkAuth, upload, uploadCloud };
