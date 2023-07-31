d9a6cd5d9a552c979a2b8c0d7ab566ff9282f716
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-31 21:55:37 +0300

    refactor routers

--- routes/api/posts-router.js
+++ routes/api/posts-router.js
@@ -1,16 +1,17 @@
 const { Router } = require('express');
 const ctrl = require('../../controllers/posts-controllers.js');
 // import * as ctrl from '../../controllers/posts-controllers';
-const { checkAuth, isValidId, uploadCloud, upload } = require('../../middlewares');
-const validateBody = require('../../decorators/validateBody');
+const { checkAuth, isValidId, uploadCloud } = require('../../middlewares');
+// const validateBody = require('../../decorators/validateBody');
 const postCreateValidation = require('../../validations/post');
 // const validate = require('../../decorators/validation');
 
 const router = Router();
 
 router.get('/', ctrl.getAll);
-router.get('/search', ctrl.getSearchPosts);
+router.get('/tags', ctrl.getAllTags);
 router.get('/tags/:tag', ctrl.getPostsByTag);
+router.get('/search', ctrl.getSearchPosts);
 router.get('/:id', isValidId, ctrl.getOne);
 
 // private
