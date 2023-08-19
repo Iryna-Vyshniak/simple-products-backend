64e1c0d1ea5df70ae2c7ef01dfc5dbf8f432b0f4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-19 14:42:39 +0300

    complete controller for get all reviews

--- controllers/review-controller/getAll.js
+++ controllers/review-controller/getAll.js
@@ -9,9 +9,31 @@ const getAll = async (req, res) => {
 
   const { page, limit, skip } = pagination(currentPage, currentLimit);
 
-  const reviews = await Review.find({}, '', { skip, limit })
-    .populate('owner', '_id name avatarUrl')
-    .sort('-createdAt');
+  // const reviews = await Review.find({}, '', { skip, limit })
+  //   .populate('owner', '_id name avatarUrl')
+  //   .sort('-createdAt');
+
+  // find owner with _id - null and return only reviews, where is really owner (not deleted)
+  // or another variant: find owner with null and then delete this review
+
+  const reviews = await Review.aggregate([
+    { $match: { owner: { $exists: true, $en: null } } },
+    { $lookup: { from: 'user', localField: 'owner', foreignField: '_id', as: 'owner' } },
+    { $unwind: '$owner' },
+    {
+      $project: {
+        _id: 1,
+        rating: 1,
+        comment: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        owner: { _id: 1, name: 1, avatarUrl: 1 },
+      },
+    },
+    { $sort: { createdAt: -1 } },
+    { $skip: skip },
+    { $limit: limit },
+  ]);
 
   if (!reviews) {
     throw HttpError(404, 'Not found reviews');
